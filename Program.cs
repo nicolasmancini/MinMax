e08bad0a86633eaee99cb606b388029b5bd583d2
Nicolas Mancini <NICOLAS.MANCINI@local.ittcesena.it>
2021-10-01 09:32:18 +0200

    V1.1: aggiunto ciclo per ricerca minimo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vettore = new int[5] { 3, 6, -3, 5, 7};
+            int min = vettore[0];
+            for(int i = 1; i < vettore.Length; i++)
+            {
+                if (vettore[i] < min)
+                    min = vettore[i];
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+
+
+
+
+
+
         }
     }
 }
